0c21b5796143692403268ba63366059fe00351c9
Geoffrey <pypaut@hotmail.fr>
2022-04-12 14:28:13 +0200

    [PYPONG] Fix & refactor Player

--- python/pygame/pypong/src/player.py
+++ python/pygame/pypong/src/player.py
@@ -1,5 +1,7 @@
 import pygame
 
+from enum import Enum
+
 class ControlType(Enum):
     PLAYER1 = 0
     PLAYER2 = 1
@@ -8,12 +10,7 @@ class Player:
     def __init__(self, rect, controls):
         self.rect = rect
         self.speed = 1
-        if controls == ControlType.PLAYER1:
-            self.K_UP = pygame.K_w
-            self.K_DOWN = pygame.K_s
-        elif controls == ControlType.PLAYER2:
-            self.K_UP = pygame.K_UP
-            self.K_DOWN = pygame.K_DOWN
+        self.set_control_keys(controls)
 
     def update(self, keys, dt, w, h):
         if keys[self.K_UP]:
@@ -23,3 +20,12 @@ class Player:
             self.rect.top += self.speed * dt
 
         self.rect = self.rect.clamp(pygame.Rect(0, 0, w, h))
+
+    def set_control_keys(self, controls):
+        if controls == ControlType.PLAYER1:
+            self.K_UP = pygame.K_w
+            self.K_DOWN = pygame.K_s
+        elif controls == ControlType.PLAYER2:
+            self.K_UP = pygame.K_UP
+            self.K_DOWN = pygame.K_DOWN
+
