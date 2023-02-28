3b63bf8951cb5626c67778ec54b1ca1a9c1b2b75
Geoffrey <pypaut@hotmail.fr>
2023-02-28 18:14:57 +0100

    [RUST][BREAKOUT] Ball/walls collision

--- rust/breakout/v2/src/ball.rs
+++ rust/breakout/v2/src/ball.rs
@@ -1,5 +1,5 @@
 use crate::components::{Ball, Direction};
-use crate::{BALL_BASE_SPEED, BALL_SIZE, TIME_STEP};
+use crate::{WinSize, BALL_BASE_SPEED, BALL_SIZE, TIME_STEP};
 use bevy::{prelude::*, sprite::MaterialMesh2dBundle};
 
 pub struct BallPlugin;
@@ -8,6 +8,7 @@ impl Plugin for BallPlugin {
     fn build(&self, app: &mut App) {
         app.add_startup_system_to_stage(StartupStage::PostStartup, ball_spawn_system);
         app.add_system(ball_movement_system);
+        app.add_system(ball_collision_system);
     }
 }
 
@@ -30,9 +31,29 @@ fn ball_spawn_system(
 fn ball_movement_system(mut query: Query<(&mut Direction, &mut Transform), With<Ball>>) {
     for (mut direction, mut transform) in query.iter_mut() {
         let translation = &mut transform.translation;
-        direction.normalize();
-
         translation.x += direction.x * TIME_STEP * BALL_BASE_SPEED;
         translation.y += direction.y * TIME_STEP * BALL_BASE_SPEED;
+    }
+}
+
+fn ball_collision_system(
+    win_size: Res<WinSize>,
+    mut query: Query<(&mut Direction, &Transform), With<Ball>>,
+) {
+    for (mut direction, mut transform) in query.iter_mut() {
+        let next_x = transform.translation.x + direction.x * TIME_STEP * BALL_BASE_SPEED;
+        let next_y = transform.translation.y + direction.y * TIME_STEP * BALL_BASE_SPEED;
+
+        // Ball reaches bottom or top
+        if next_y - BALL_SIZE / 2. < -win_size.h / 2. || next_y + BALL_SIZE / 2. > win_size.h / 2. {
+            direction.y *= -1.;
+        }
+
+        // Ball reaches left or right
+        if next_x - BALL_SIZE / 2. < -win_size.w / 2. || next_x + BALL_SIZE / 2. > win_size.w / 2. {
+            direction.x *= -1.;
+        }
+
+        direction.normalize();
     }
 }
