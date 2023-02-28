97e419b0a2eae969758c389a18ee19661c547e7f
Geoffrey <pypaut@hotmail.fr>
2023-02-28 19:19:00 +0100

    [RUST][BREAKOUT] Ball/player collision

--- rust/breakout/v2/src/ball.rs
+++ rust/breakout/v2/src/ball.rs
@@ -1,5 +1,5 @@
-use crate::components::{Ball, Direction};
-use crate::{WinSize, BALL_BASE_SPEED, BALL_SIZE, TIME_STEP};
+use crate::components::{Ball, Direction, Player};
+use crate::{WinSize, BALL_BASE_SPEED, BALL_SIZE, PLAYER_SIZE, TIME_STEP};
 use bevy::{prelude::*, sprite::MaterialMesh2dBundle};
 
 pub struct BallPlugin;
@@ -8,7 +8,8 @@ impl Plugin for BallPlugin {
     fn build(&self, app: &mut App) {
         app.add_startup_system_to_stage(StartupStage::PostStartup, ball_spawn_system);
         app.add_system(ball_movement_system);
-        app.add_system(ball_collision_system);
+        app.add_system(ball_wall_collision_system);
+        app.add_system(ball_player_collision_system);
     }
 }
 
@@ -29,18 +30,18 @@ fn ball_spawn_system(
 }
 
 fn ball_movement_system(mut query: Query<(&mut Direction, &mut Transform), With<Ball>>) {
-    for (mut direction, mut transform) in query.iter_mut() {
+    for (direction, mut transform) in query.iter_mut() {
         let translation = &mut transform.translation;
         translation.x += direction.x * TIME_STEP * BALL_BASE_SPEED;
         translation.y += direction.y * TIME_STEP * BALL_BASE_SPEED;
     }
 }
 
-fn ball_collision_system(
+fn ball_wall_collision_system(
     win_size: Res<WinSize>,
     mut query: Query<(&mut Direction, &Transform), With<Ball>>,
 ) {
-    for (mut direction, mut transform) in query.iter_mut() {
+    for (mut direction, transform) in query.iter_mut() {
         let next_x = transform.translation.x + direction.x * TIME_STEP * BALL_BASE_SPEED;
         let next_y = transform.translation.y + direction.y * TIME_STEP * BALL_BASE_SPEED;
 
@@ -55,5 +56,38 @@ fn ball_collision_system(
         }
 
         direction.normalize();
+    }
+}
+
+fn ball_player_collision_system(
+    mut ball_query: Query<(&mut Direction, &Transform), With<Ball>>,
+    mut player_query: Query<&Transform, With<Player>>,
+) {
+    for (mut ball_dir, ball_transform) in ball_query.iter_mut() {
+        let next_ball_x = ball_transform.translation.x + ball_dir.x * TIME_STEP * BALL_BASE_SPEED;
+        let next_ball_y = ball_transform.translation.y + ball_dir.y * TIME_STEP * BALL_BASE_SPEED;
+
+        if let Ok(player_transform) = player_query.get_single_mut() {
+            let player_x = player_transform.translation.x;
+            let player_y = player_transform.translation.y;
+
+            let player_left = player_x - PLAYER_SIZE.0 / 2.;
+            let player_right = player_x + PLAYER_SIZE.0 / 2.;
+
+            let player_bot = player_y - PLAYER_SIZE.1 / 2.;
+            let player_top = player_y + PLAYER_SIZE.1 / 2.;
+
+            // Check ball/player collision
+            let x_cond = player_left < next_ball_x && next_ball_x < player_right;
+            let y_cond = player_bot < next_ball_y && next_ball_y < player_top;
+            if x_cond && y_cond {
+                let c = (ball_transform.translation.x - player_x) / PLAYER_SIZE.0;
+
+                ball_dir.y *= -1.;
+                ball_dir.x = c;
+
+                ball_dir.normalize();
+            }
+        }
     }
 }
