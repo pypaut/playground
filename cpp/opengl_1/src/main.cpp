736db71e9fcd6aeec30f2c476a230e4db76e840f
Geoffrey <pypaut@hotmail.fr>
2023-08-19 12:22:45 +0200

    [CPP][OPENGL] Use index buffer

--- cpp/opengl_1/src/main.cpp
+++ cpp/opengl_1/src/main.cpp
@@ -113,20 +113,31 @@ int main(void) {
     std::cout << "OpenGL version: " << glGetString(GL_VERSION) << std::endl;
 
     /* Data buffer */
-    float positions[6] = {
+    float positions[8] = {
         -0.5f, -0.5f,
-         0.0f,  0.5f,
+         0.5f,  0.5f,
          0.5f, -0.5f,
+        -0.5f,  0.5f,
+    };
+
+    unsigned int indices[] = {
+        0, 1, 2,
+        0, 1, 3,
     };
 
     /* Setup data buffer to send input to shaders */
     unsigned int buffer;
     glGenBuffers(1, &buffer);
     glBindBuffer(GL_ARRAY_BUFFER, buffer);
-    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, 8 * sizeof(float), positions, GL_STATIC_DRAW);
 
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+
+    unsigned int ibo;
+    glGenBuffers(1, &ibo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW);
 
     ShaderProgramSource source = ParseShaders("res/shaders/basic.shader");
     unsigned int program = CreateProgram(source.VertexSource, source.FragmentSource);
@@ -137,7 +148,7 @@ int main(void) {
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
