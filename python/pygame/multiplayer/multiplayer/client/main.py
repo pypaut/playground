7b0d65a142100166512051dc794d2f4f7dc0ccbf
Geoffrey <pypaut@hotmail.fr>
2024-07-23 17:40:10 +0200

    [PY][MULTI] Basic client with PyGame & config

--- python/pygame/multiplayer/multiplayer/client/main.py
+++ python/pygame/multiplayer/multiplayer/client/main.py
@@ -1,5 +1,53 @@
+import configparser
+import pygame
+
+from pygame.locals import QUIT, KEYDOWN
+
+
 def main():
-    print("Client!")
+    config = configparser.ConfigParser()
+    with open('config.ini', 'r') as configfile:
+        config.read_file(configfile)
+
+    H = config.getint('CLIENT', 'H')
+    W = config.getint('CLIENT', 'W')
+    FPS = config.getint('CLIENT', 'FPS')
+
+    # Init
+    pygame.display.init()
+    pygame.font.init()
+    pygame.key.set_repeat(1, 1)
+    pygame.display.set_caption("Multiplayer")
+    window = pygame.display.set_mode((W, H))
+    clock = pygame.time.Clock()
+
+    while True:
+        clock.tick(FPS)
+
+        # Quit event
+        events = pygame.event.get()
+        if QUIT in [e.type for e in events]:
+            break
+
+        # Keyboard events
+        if KEYDOWN in [e.type for e in events]:
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_ESCAPE]:
+                break
+
+            player_dir = [0, 0]
+
+            if keys[pygame.K_w]:
+                player_dir[1] -= 1
+            if keys[pygame.K_a]:
+                player_dir[0] -= 1
+            if keys[pygame.K_s]:
+                player_dir[1] += 1
+            if keys[pygame.K_d]:
+                player_dir[0] += 1
+
+            print(player_dir)
+
 
 if __name__ == "__main__":
     pass
