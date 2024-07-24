7e79dcfd3b2649c083b278213988941ebdfa1691
Geoffrey <pypaut@hotmail.fr>
2024-07-24 16:58:18 +0200

    [PY][MULTI] Refactor client into class

---
+++ python/pygame/multiplayer/multiplayer/client/client.py
@@ -0,0 +1,53 @@
+import configparser
+import pygame
+
+from pygame.locals import QUIT, KEYDOWN
+
+class Client:
+    def __init__(self):
+        # Read config
+        config = configparser.ConfigParser()
+        with open('config.ini', 'r') as configfile:
+            config.read_file(configfile)
+
+        self.H = config.getint('CLIENT', 'H')
+        self.W = config.getint('CLIENT', 'W')
+        self.FPS = config.getint('CLIENT', 'FPS')
+
+        # Init PyGame
+        pygame.display.init()
+        pygame.font.init()
+        pygame.key.set_repeat(1, 1)
+        pygame.display.set_caption("Multiplayer")
+
+        self.window = pygame.display.set_mode((self.W, self.H))
+        self.clock = pygame.time.Clock()
+
+    def run(self):
+        while True:
+            self.clock.tick(self.FPS)
+
+            # Quit event
+            events = pygame.event.get()
+            if QUIT in [e.type for e in events]:
+                break
+
+            # Keyboard events
+            if KEYDOWN in [e.type for e in events]:
+                keys = pygame.key.get_pressed()
+                if keys[pygame.K_ESCAPE]:
+                    break
+
+                player_dir = [0, 0]
+
+                if keys[pygame.K_w]:
+                    player_dir[1] -= 1
+                if keys[pygame.K_a]:
+                    player_dir[0] -= 1
+                if keys[pygame.K_s]:
+                    player_dir[1] += 1
+                if keys[pygame.K_d]:
+                    player_dir[0] += 1
+
+                print(player_dir)
+
--- python/pygame/multiplayer/multiplayer/client/main.py
+++ python/pygame/multiplayer/multiplayer/client/main.py
@@ -1,52 +1,9 @@
-import configparser
-import pygame
-
-from pygame.locals import QUIT, KEYDOWN
+from multiplayer.client.client import Client
 
 
 def main():
-    config = configparser.ConfigParser()
-    with open('config.ini', 'r') as configfile:
-        config.read_file(configfile)
-
-    H = config.getint('CLIENT', 'H')
-    W = config.getint('CLIENT', 'W')
-    FPS = config.getint('CLIENT', 'FPS')
-
-    # Init
-    pygame.display.init()
-    pygame.font.init()
-    pygame.key.set_repeat(1, 1)
-    pygame.display.set_caption("Multiplayer")
-    window = pygame.display.set_mode((W, H))
-    clock = pygame.time.Clock()
-
-    while True:
-        clock.tick(FPS)
-
-        # Quit event
-        events = pygame.event.get()
-        if QUIT in [e.type for e in events]:
-            break
-
-        # Keyboard events
-        if KEYDOWN in [e.type for e in events]:
-            keys = pygame.key.get_pressed()
-            if keys[pygame.K_ESCAPE]:
-                break
-
-            player_dir = [0, 0]
-
-            if keys[pygame.K_w]:
-                player_dir[1] -= 1
-            if keys[pygame.K_a]:
-                player_dir[0] -= 1
-            if keys[pygame.K_s]:
-                player_dir[1] += 1
-            if keys[pygame.K_d]:
-                player_dir[0] += 1
-
-            print(player_dir)
+    client = Client()
+    client.run()
 
 
 if __name__ == "__main__":
