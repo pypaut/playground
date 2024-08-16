6a3d49733c10a1273673cc2f13305c03b315334c
Geoffrey <pypaut@hotmail.fr>
2024-08-16 11:58:15 +0200

    [C][LIST] Simplify test return value

--- c/list/test/main.c
+++ c/list/test/main.c
@@ -1,15 +1,13 @@
 #include "test.h"
 
 int main() {
-    int ret_value = 0;
-
     if (test_new_list()) {
-        ret_value = 1;
+        return 1;
     }
 
     if (test_free_list()) {
-        ret_value = 1;
+        return 1;
     }
 
-    return ret_value;
+    return 0;
 }
--- c/list/test/test_free_list.c
+++ c/list/test/test_free_list.c
@@ -2,11 +2,10 @@
 
 int test_free_list() {
     log_title("free_list()");
-    int ret_value = 0;
 
     struct list *my_list = new_list();
     free_list(my_list);
     log_success("should not throw error lol");
 
-    return ret_value;
+    return 0;
 }
