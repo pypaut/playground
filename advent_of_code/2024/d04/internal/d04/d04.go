9bf889880dfbae883cba4b5602e3b5892dd7d95d
Geoffrey <pypaut@hotmail.fr>
2024-12-17 17:10:06 +0100

    [AOC][D4] P2: rm redundant conditions

--- advent_of_code/2024/d04/internal/d04/d04.go
+++ advent_of_code/2024/d04/internal/d04/d04.go
@@ -11,10 +11,8 @@ func CountMasInX(lines []string) (nbXmas int) {
 						lines[lineNb+1][charNb+1] == 'A' &&
 						lines[lineNb+2][charNb+2] == 'M') {
 					if (lines[lineNb][charNb+2] == 'M' &&
-						lines[lineNb+1][charNb+1] == 'A' &&
 						lines[lineNb+2][charNb] == 'S') ||
 						(lines[lineNb][charNb+2] == 'S' &&
-							lines[lineNb+1][charNb+1] == 'A' &&
 							lines[lineNb+2][charNb] == 'M') {
 						nbXmas++
 					}
