d76685ac604be87038e89015413027ab819fe0ea
Geoffrey <pypaut@hotmail.fr>
2025-06-23 13:25:40 +0200

    [GO][PONGOPS] Fix first iteration of client/server

--- go/pongops/server/server.go
+++ go/pongops/server/server.go
@@ -1,8 +1,8 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"io"
 	"log"
 	"net"
 )
@@ -24,17 +24,22 @@ func runServer() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer conn.Close()
+	defer func(conn net.Conn) {
+		err := conn.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}(conn)
 
 	for {
-		// n, err := conn.Read(buf.Bytes())
-		message, err := io.ReadAll(conn)
+		/* Receive player's direction from client */
+		message, err := bufio.NewReader(conn).ReadBytes('\n')
 		if err != nil {
 			log.Fatal(err)
 		}
 		fmt.Printf("Received message '%s'\n", string(message))
 
-		n, err := fmt.Fprintf(conn, "%s", message)
+		n, err := fmt.Fprintf(conn, "%s", string(message))
 		if n == 0 {
 			fmt.Println("Sent empty message")
 		}
@@ -43,159 +48,3 @@ func runServer() {
 		}
 	}
 }
-
-// Init game, with objects
-// Accept connections (2)
-//
-
-/*
-func main() {
-	gameObjects := CreateGameObjects()
-
-	ln, err := net.Listen("tcp", ":8000")
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("Listening on port 8000")
-
-	conn1, err := ln.Accept()
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("Player 1 is connected")
-	go handlePlayer(conn1, gameObjects["player1"], 1, gameObjects)
-
-	conn2, err := ln.Accept()
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("Player 2 is connected")
-	go handlePlayer(conn2, gameObjects["player2"], 2, gameObjects)
-
-	var buf bytes.Buffer
-	enc := gob.NewEncoder(&buf)
-
-	for {
-		// Wait one frame
-		time.Sleep(1 / 60 * time.Second)
-
-		// Update each object according to direction, collision
-		Update(gameObjects)
-
-		if err = enc.Encode(gameObjects); err != nil {
-			fmt.Println("Error encoding game objects:", err)
-			return
-		}
-
-		_, err = conn1.Write(buf.Bytes())
-		if err != nil {
-			return
-		}
-
-		_, err = conn1.Write(buf.Bytes())
-		if err != nil {
-			return
-		}
-	}
-}
-
-func handlePlayer(conn net.Conn, player *structs.Object, playerID int, objects map[string]*structs.Object) {
-	// var readBuf bytes.Buffer
-	var playerDir structs.Direction
-
-	var writeBuf bytes.Buffer
-	enc := gob.NewEncoder(&writeBuf)
-
-	for {
-		// Wait one frame
-		time.Sleep(1 / 60 * time.Second)
-
-		err := receivePlayerDir(conn, &playerDir)
-		if err != nil {
-			fmt.Printf("Player %d disconnected\n", playerID)
-		}
-
-		// Update player object with direction
-		player.DirX, player.DirY = playerDir.X, playerDir.Y
-
-		if err = enc.Encode(objects); err != nil {
-			fmt.Println("Error encoding encoding objects:", err)
-			return
-		}
-
-		n, err := fmt.Fprintf(conn, writeBuf.String()+"\n")
-		if n == 0 {
-			fmt.Println("No data written")
-		}
-		if err != nil {
-			fmt.Println("Error sending data:", err)
-		}
-	}
-}
-
-func receivePlayerDir(conn net.Conn, playerDir *structs.Direction) (err error) {
-	/* Receive player's direction from client */
-// n, err := conn.Read(readBuf.Bytes())
-// if n == 0 {
-// 	fmt.Printf("Player %d disconnected\n", playerID)
-// 	return
-// }
-// if err != nil {
-// 	log.Fatal(err)
-// }
-
-/*
-	playerDirByte, err := bufio.NewReader(conn).ReadBytes('\n')
-	if err != nil {
-		return
-	}
-
-	dec := gob.NewDecoder(bytes.NewBuffer(playerDirByte))
-	// dec := gob.NewDecoder(&readBuf)
-	if err = dec.Decode(&playerDir); err != nil {
-		fmt.Println("Error decoding struct:", err)
-		return
-	}
-
-	return nil
-}
-
-func Update(gameObjects map[string]*structs.Object) {
-	for _, obj := range gameObjects {
-		obj.PosX += obj.DirX
-		obj.PosY += obj.DirY
-	}
-
-	return
-}
-
-func CreateGameObjects() map[string]*structs.Object {
-	player1 := structs.Object{
-		PosX:   200,
-		PosY:   0,
-		Width:  50,
-		Height: 300,
-	}
-
-	player2 := structs.Object{
-		PosX:   structs.WinW - 200,
-		PosY:   0,
-		Width:  50,
-		Height: 300,
-	}
-
-	ball := structs.Object{
-		PosX:   structs.WinW / 2,
-		PosY:   structs.WinH / 2,
-		Width:  20,
-		Height: 20,
-	}
-
-	return map[string]*structs.Object{
-		"player1": &player1,
-		"player2": &player2,
-		"ball":    &ball,
-	}
-}
-
-*/
--- go/pongops/server/server_test.go
+++ go/pongops/server/server_test.go
@@ -1,57 +1,33 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"io"
 	"net"
 	"testing"
 )
 
-// func TestReceivePlayerDir(t *testing.T) {
-// 	// Create a server
-// 	ts := httptest.NewTLSServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-// 		fmt.Fprintln(w, "Hello, client")
-// 	}))
-// 	defer ts.Close()
-//
-// 	// Create a client
-// 	client := ts.Client()
-// 	res, err := client.Get(ts.URL)
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-//
-// 	greeting, err := io.ReadAll(res.Body)
-// 	res.Body.Close()
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-//
-// 	// fmt.Printf("%s", greeting)
-// 	if string(greeting) != "Hello, client\n" {
-// 		t.Fatalf("Expected '%s', got '%s'", "Hello, client", string(greeting))
-// 	}
-// }
-
 func TestServer(t *testing.T) {
 	go runServer()
+
+	messageToSend := "my message hehe\n"
 
 	conn, err := net.Dial("tcp", "localhost:"+PORT)
 	if err != nil {
 		t.Fatalf("Could not connect to the server: %v", err)
 	}
 
-	n, err := fmt.Fprintf(conn, "hello world")
+	n, err := fmt.Fprint(conn, messageToSend)
 	if n == 0 || err != nil {
 		t.Fatalf("Could not send a message to the server: %v", err)
 	}
 
-	message, err := io.ReadAll(conn)
+	receivedMessage, err := bufio.NewReader(conn).ReadBytes('\n')
 	if err != nil {
 		t.Fatalf("Could not read a message from the server: %v", err)
 	}
 
-	if string(message) != "hello world" {
-		t.Fatalf("Unexpected message: %s", message)
+	if string(receivedMessage) != messageToSend {
+		t.Fatalf("Unexpected message: %s", receivedMessage)
 	}
 }
