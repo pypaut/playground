6eb7aec274b6350a37d51348198de89a2cfaaa65
Geoffrey <pypaut@hotmail.fr>
2025-10-02 17:34:55 +0200

    [GO][COMPTES] Parse date from arguments

--- go/comptes/internal/cli.go
+++ go/comptes/internal/cli.go
@@ -1,5 +1,10 @@
 package internal
 
+import (
+	"fmt"
+	"time"
+)
+
 type CLI struct {
 	Budgets  BudgetsCmd  `cmd:"budgets" help:"Show budgets"`
 	Expenses ExpensesCmd `cmd:"accounts" help:"Show expenses"`
@@ -18,9 +23,15 @@ func NewCli(datastore *Datastore) *CLI {
 
 type BudgetsCmd struct {
 	datastore *Datastore
+
+	Year  int
+	Month int
 }
 
 func (cmd *BudgetsCmd) Run() error {
+	date := parseDate(cmd.Year, cmd.Month)
+	fmt.Printf("Date: %v\n", date)
+
 	incomesTable := BuildIncomesTable(cmd.datastore)
 	budgetsTables := BuildBudgetsTables(cmd.datastore)
 	remainTable := BuildRemainTable(cmd.datastore)
@@ -42,4 +53,19 @@ func (cmd *ExpensesCmd) Run() error {
 	expensesTable := BuildExpensesTable(cmd.datastore)
 	expensesTable.Render()
 	return nil
+}
+
+func parseDate(year int, month int) string {
+	now := time.Now()
+	if year == 0 {
+		year = now.Year()
+	}
+
+	if month == 0 {
+		month = int(now.Month())
+	}
+
+	return time.Date(
+		year, time.Month(month), 1, 0, 0, 0, 0, time.Local,
+	).Format("2006-01")
 }
