a9b4c495842491a13b5c6ee42690dd538cf2459a
Geoffrey <pypaut@hotmail.fr>
2025-12-10 16:02:01 +0100

    aoc: d03, part 2

--- advent_of_code/2025/d03/main.go
+++ advent_of_code/2025/d03/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"log"
+	"math"
 	"os"
 	"strconv"
 )
@@ -11,6 +13,10 @@ import (
 func main() {
 	maxJoltage := ComputePart1("input")
 	fmt.Println(maxJoltage)
+
+	maxJoltage = ComputePart2("input")
+	fmt.Println(maxJoltage)
+
 	return
 }
 
@@ -32,6 +38,49 @@ func ComputePart1(path string) (maxJoltage int) {
 	return
 }
 
+func ComputePart2(path string) (maxJoltage int) {
+	file, err := os.Open(path)
+	if err != nil {
+		panic(err)
+	}
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		maxJoltage += GetMaxJoltagePart2(scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	return
+}
+
+func GetMaxJoltagePart2(bank string) (maxJoltage int) {
+	intBank := []int{}
+	for _, c := range bank {
+		intVal, err := strconv.Atoi(string(c))
+		if err != nil {
+			panic(err)
+		}
+
+		intBank = append(intBank, intVal)
+	}
+
+	l := len(bank)
+	i := -1
+	m := intBank[0]
+
+	for counter := 11; counter >= 0; counter-- {
+		left := i + 1
+		right := l - counter
+		m, i = MaxWithIndice(intBank, left, right)
+		maxJoltage += m * int(math.Pow10(counter))
+	}
+
+	return
+}
+
 func GetMaxJoltage(bank string) int {
 	intBank := []int{}
 	for _, c := range bank {
@@ -44,26 +93,27 @@ func GetMaxJoltage(bank string) int {
 	}
 
 	// Find max diggit and its indice
-	maxValue, maxIndex := MaxWithIndice(intBank[:len(intBank)-1])
+	maxValue, maxIndex := MaxWithIndice(intBank, 0, len(intBank)-1)
 
 	// Find max diggit, placed after this indice
-	secondMaxValue, _ := MaxWithIndice(intBank[maxIndex+1:])
+	secondMaxValue, _ := MaxWithIndice(intBank, maxIndex+1, len(intBank))
 
 	return maxValue*10 + secondMaxValue
 }
 
-func MaxWithIndice(s []int) (value, index int) {
-	if len(s) == 0 {
-		return
+func MaxWithIndice(s []int, l int, r int) (value, index int) {
+	if len(s) == 0 || l >= r || l >= len(s) || r > len(s) {
+		panic(errors.New("wrong max call"))
 	}
 
-	value = s[0]
-	for i, v := range s {
-		if v > value {
-			value = v
+	index = l
+	value = s[index]
+	for i := l; i < r; i++ {
+		if s[i] > value {
+			value = s[i]
 			index = i
 		}
 	}
 
-	return
+	return value, index
 }
--- advent_of_code/2025/d03/main_test.go
+++ advent_of_code/2025/d03/main_test.go
@@ -39,4 +39,89 @@ func TestSample(t *testing.T) {
 	expectedJoltage := 357
 	totalJoltage := ComputePart1("sample")
 	assert.Equal(t, totalJoltage, expectedJoltage)
+
+	expectedJoltage = 3121910778619
+	totalJoltage = ComputePart2("sample")
+	assert.Equal(t, totalJoltage, expectedJoltage)
+}
+
+func TestGetMaxJoltagePart2(t *testing.T) {
+	cases := []struct {
+		bank       string
+		maxJoltage int
+	}{
+		{
+			bank:       "987654321111111",
+			maxJoltage: 987654321111,
+		},
+		{
+			bank:       "811111111111119",
+			maxJoltage: 811111111119,
+		},
+		{
+			bank:       "234234234234278",
+			maxJoltage: 434234234278,
+		},
+		{
+			bank:       "818181911112111",
+			maxJoltage: 888911112111,
+		},
+	}
+
+	for _, c := range cases {
+		joltage := GetMaxJoltagePart2(c.bank)
+		assert.Equal(t, joltage, c.maxJoltage)
+	}
+}
+
+func TestMaxWithIndice(t *testing.T) {
+	cases := []struct {
+		s              []int
+		left           int
+		right          int
+		expectedMax    int
+		expectedIndice int
+	}{
+		{
+			s:              []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9},
+			left:           0,
+			right:          10,
+			expectedMax:    9,
+			expectedIndice: 9,
+		},
+		{
+			s:              []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9},
+			left:           9,
+			right:          10,
+			expectedMax:    9,
+			expectedIndice: 9,
+		},
+		{
+			s:              []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9},
+			left:           5,
+			right:          6,
+			expectedMax:    5,
+			expectedIndice: 5,
+		},
+		{
+			s:              []int{2, 3, 4, 2, 3, 4, 2, 3, 4, 2, 3, 4, 2, 7, 8},
+			left:           0,
+			right:          4,
+			expectedMax:    4,
+			expectedIndice: 2,
+		},
+		{
+			s:              []int{2, 3, 4, 2, 3, 4, 2, 3, 4, 2, 3, 4, 2, 7, 8},
+			left:           3,
+			right:          5,
+			expectedMax:    3,
+			expectedIndice: 4,
+		},
+	}
+
+	for _, c := range cases {
+		m, i := MaxWithIndice(c.s, c.left, c.right)
+		assert.Equal(t, m, c.expectedMax)
+		assert.Equal(t, i, c.expectedIndice)
+	}
 }
