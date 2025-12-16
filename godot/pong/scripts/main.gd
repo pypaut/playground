26593c91742c78f3439f75ff97539b064ea03650
Geoffrey <pypaut@hotmail.fr>
2025-12-16 18:23:23 +0100

    godot(pong): mute unused param

--- godot/pong/scripts/main.gd
+++ godot/pong/scripts/main.gd
@@ -1,9 +1,9 @@
 extends Sprite2D
 
 
-func _on_left_wall_body_entered(body: Node2D) -> void:
+func _on_left_wall_body_entered(_body: Node2D) -> void:
 	get_tree().quit()
 
 
-func _on_right_wall_body_entered(body: Node2D) -> void:
+func _on_right_wall_body_entered(_body: Node2D) -> void:
 	get_tree().quit()
