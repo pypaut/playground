b15628834bb9562974a3a013117e17fbc2e36ce7
Geoffrey <pypaut@hotmail.fr>
2026-01-26 21:57:13 +0100

    godot(scarycats): import jack

--- godot/scarycat/scripts/world.gd
+++ godot/scarycat/scripts/world.gd
@@ -3,14 +3,21 @@ extends Node3D
 var KIRA_VISIBLE = false
 var KIRA_SPEED = 1000
 
+
 func _ready() -> void:
+	# $MenuCamera.make_current()
 	pass
 
 
 func _process(delta: float) -> void:
 	var character_pos = $Character.global_position
+	
+	# Sprites look at player
 	$Nala.look_at(character_pos)
 	$Kira.look_at(character_pos)
+	$Jack.look_at(character_pos)
+	
+	# Move Kira
 	if KIRA_VISIBLE:
 		var kira_direction = $Kira.global_position.direction_to(character_pos)
 		$Kira.velocity = kira_direction * KIRA_SPEED * delta
