1b21deb424cde4b10c81ac41c246ada0ffe02089
Geoffrey <pypaut@hotmail.fr>
2026-01-29 09:11:08 +0100

    godot(scarycats): refactor hud and interact with jack

--- godot/scarycat/scripts/character.gd
+++ godot/scarycat/scripts/character.gd
@@ -6,31 +6,57 @@ const JUMP_VELOCITY = 10.0
 const CAMERA_SENSITIVITY = 10
 const MAX_CAMERA_ANGLE = 1.5
 
+@onready var raycast: RayCast3D = $Camera3D/RayCast3D
+
+var is_interacting: bool = false
+
 
 func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+
+func _process(_delta: float):
+	if raycast.is_colliding():
+		var interactable = raycast.get_collider()
+		if interactable and interactable.has_method("jack"):
+			$HUD/InteractionText.text = "E"
+			is_interacting = true
+		else:
+			$HUD/InteractionText.text = ""
+			is_interacting = false
+	else:
+		$HUD/InteractionText.text = ""
+		is_interacting = false
+
+
+func _input(event):
+	if is_interacting and event.is_action_pressed("interact"):
+		var interactable = raycast.get_collider()
+
+		# Jack
+		if interactable and interactable.has_method("jack"):
+			$HUD/DialogueText.text = "I'm just resting here."
 
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
 		# Rotate the whole character left and right
 		rotation.y = rotation.y - event.relative.x * 1/(CAMERA_SENSITIVITY*10)
+
 		# Rotate the camera only up and down
 		$Camera3D.rotation.x = $Camera3D.rotation.x - event.relative.y * 1/(CAMERA_SENSITIVITY*10)
 		$Camera3D.rotation.x = clamp($Camera3D.rotation.x, -MAX_CAMERA_ANGLE, MAX_CAMERA_ANGLE)
 
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	# Add the gravity
 	if not is_on_floor():
 		velocity += get_gravity() * delta * 3.0
 
-	# Handle jump.
+	# Handle jump
 	if Input.is_action_just_pressed("space") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and han dle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("left", "right", "up", "down")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
---
+++ godot/scarycat/scripts/jack.gd
@@ -0,0 +1,7 @@
+extends Area3D
+
+
+# This method is only here for the "has_method()" call in the character script
+# This way I can recognize this node
+func jack():
+	pass
